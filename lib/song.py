03615b1fa7015383f109214a6a5f28761b242409
alex-7-njoroge <njorogekinyanjui02@gmail.com>
2024-09-19 11:03:36 +0300

    lab done

--- lib/song.py
+++ lib/song.py
@@ -6,10 +6,15 @@ class Song:
     artist_count = {}
 
     def __init__(self, name, artist, genre):
+        # Instance attributes
         self.name = name
         self.artist = artist
         self.genre = genre
-        self.add_song_to_count()
+        
+        # Increment the song count when a new song is created
+        self.add_song_to_count()   
+        
+        # Add the genre and artist to the respective lists and update counts
         self.add_to_genres(genre)
         self.add_to_artists(artist)
         self.add_to_genre_count(genre)
@@ -17,20 +22,24 @@ class Song:
 
     @classmethod
     def add_song_to_count(cls):
+        """Increment the song count."""
         cls.count += 1
 
     @classmethod
     def add_to_genres(cls, genre):
+        """Add a new genre to the genres list if it is not already present."""
         if genre not in cls.genres:
             cls.genres.append(genre)
 
     @classmethod
     def add_to_artists(cls, artist):
+        """Add a new artist to the artists list if it is not already present."""
         if artist not in cls.artists:
             cls.artists.append(artist)
 
     @classmethod
     def add_to_genre_count(cls, genre):
+        """Increment the count of songs for a specific genre."""
         if genre in cls.genre_count:
             cls.genre_count[genre] += 1
         else:
@@ -38,7 +47,10 @@ class Song:
 
     @classmethod
     def add_to_artist_count(cls, artist):
+        """Increment the count of songs for a specific artist."""
         if artist in cls.artist_count:
             cls.artist_count[artist] += 1
         else:
             cls.artist_count[artist] = 1
+    pass
+  
